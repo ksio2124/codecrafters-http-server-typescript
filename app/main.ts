2465076d7c35b4d30051d9d8a674a81da996fe98
jack <ksio2124@protonmail.ch>
2025-06-11 12:18:23 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,13 +6,49 @@ console.log("Logs from your program will appear here!");
 class HTTPRequest {
   header: string;
   method: string;
-  path: string;
+  path: string[];
   version: string;
   constructor( data: string){
     // Parse the HTTP request data
     const lines = data.split("\r\n");
     this.header = lines[0];
-    [this.method, this.path, this.version] = this.header.split(" ");
+    let fullPath = '';
+    [this.method, fullPath, this.version] = this.header.split(" ");
+    this.path = fullPath.split("/");
+  }
+}
+
+class HTTPResponse {
+  statusCode: number;
+  statusMessage: string;
+  header: HTTPHeader;
+  body: string;
+  constructor(statusCode: number, statusMessage: string, body: string, header: HTTPHeader) {
+    this.header = header;
+    this.body = body;
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage;
+  }
+
+  toString() {
+    return `HTTP/1.1 ${this.statusCode} ${this.statusMessage}\r\n${this.header.getHeadersAsString()}\r\n\r\n${this.body}`;
+  }
+}
+class HTTPHeader {
+  headers: Record<string, string>;
+  constructor() {
+    this.headers = {};
+  }
+  addHeader(name: string, value: string) {
+    this.headers[name] = value;
+  }
+  getheaders() {
+    return this.headers;
+  }
+  getHeadersAsString() {
+    return Object.entries(this.headers)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join("\r\n");
   }
 }
 
@@ -25,11 +61,18 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     // console.log("Received data:", data.toString());
     const request = new HTTPRequest(data.toString());
-    if (request.method === "GET" && request.path === "/") {
+    console.log("path: ",request.path);
+    if (request.method === "GET" && request.path.length === 2 && request.path[0] === "" && request.path[1] === "") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      return
+    } else if (request.method === "GET" && request.path[1] === "echo") {
+      const reponseHeader = new HTTPHeader();
+      reponseHeader.addHeader("Content-Type", "text/plain");
+      reponseHeader.addHeader("Content-Length", request.path[2].length.toString());
+      const response = new HTTPResponse(200, "OK", request.path[2], reponseHeader);
+      socket.write(response.toString());
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
-    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     socket.end();
   });
 });
