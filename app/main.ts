ca05017d0d415c3622bdb095a47590d8046eac89
jack <ksio2124@protonmail.ch>
2025-06-11 11:41:43 -0400

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,8 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket
   socket.on("close", () => {
     socket.end();
+  });
+  socket.on("data", (data) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
   });
 });
 
